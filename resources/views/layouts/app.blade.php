54e5d35729786655d18ce5df8348ef53ffabeb4b
santiagoperatta <santiagoperattawpp@gmail.com>
2023-09-18 20:15:42 -0300

    Actualizar archivos de idioma y diseño de la aplicación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+		@stack('styles')
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -13,7 +13,6 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 		@livewireStyles
-		@stack('styles')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen">
