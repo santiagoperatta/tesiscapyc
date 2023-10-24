bb245d0a5501892081525a502998c593cbe10f8d
santiagoperatta <santiagoperattawpp@gmail.com>
2023-10-24 13:14:21 -0300

    final

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,5 +1,4 @@
-<form method="POST" class="1/2 space-y-5" wire:submit.prevent='crearVacante'>
-	@csrf
+<form class="md:w-1/2 space-y-5" wire:submit.prevent='crearVacante'>
 	<div>
 		<x-input-label for="titulo" :value="__('Titulo Vacante')" />
 		<x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" placeholder="Titulo vacante" :value="old('titulo')"/>
